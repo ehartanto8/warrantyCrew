bd3c1022429b295c57be28fc942a53e35b97c25b
Edgar Hartanto <ehartanto8@gmail.com>
2025-09-03 20:57:59 -0400

    loaders

---
+++ embedchain/loaders/directory_loader.py
@@ -0,0 +1,7 @@
+class DirectoryLoader:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def load(self, *args, **kwargs):
+        # return an empty list so imports succeed; you’re not using this tool
+        return []
